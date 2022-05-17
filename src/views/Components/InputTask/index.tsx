6db941ea963e5779d6eed604b56a5e0312b2e250
Yaroslav Shostak <yaroslavshostak4@gmail.com>
2022-05-17 15:36:45 +0300

    Stylized taskItem

--- src/views/Components/InputTask/index.tsx
+++ src/views/Components/InputTask/index.tsx
@@ -11,10 +11,41 @@ interface InputTaskProps {
 }
 
 export const InputTask: React.FC<InputTaskProps> = (
-    {id, title, onDone, onEdited, onRemoved}) => {
+    {id, title, onDone, onEdited, onRemoved}
+) => {
+    const [checked, setChecked] = useState(false);
     return (
         <div className={styles.inputTask}>
-            {title}
+            <label className={styles.inputTaskLabel}>
+                <input 
+                    type="checkbox" 
+                    checked={checked}
+                    className={styles.inputTaskCheckbox}
+                    onChange={(event) => {
+                        setChecked(event.target.checked);
+                        if(event.target.checked) {
+                            onDone(id);
+                        }
+                    }}
+                />
+                <h3 className={styles.inputTaskTitle}>{title}</h3>
+            </label>
+            <button 
+                aria-label="Edit"
+                className={styles.inputTaskEdit}
+                onClick={() => {
+
+                }}
+            />
+            <button 
+                aria-label="Remove"
+                className={styles.inputTaskRemove}
+                onClick={() => {
+                    if(confirm('Are you sure?')) {
+                        onRemoved(id);
+                    }
+                }}
+            /> 
         </div>
     );
 }
